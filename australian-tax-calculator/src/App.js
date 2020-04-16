e395cb6832a9d8dbe65249a4022c670fe7522e1b
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-04-16 15:03:59 +0930

    Updated css

--- australian-tax-calculator/src/App.js
+++ australian-tax-calculator/src/App.js
@@ -5,17 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
       </header>
     </div>
   );
