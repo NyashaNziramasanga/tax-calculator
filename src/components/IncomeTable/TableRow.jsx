6ffde567a1c032b59961333fdf97dbd972a09b0f
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-04-16 20:55:50 +0930

    Updated table length

--- src/components/IncomeTable/TableRow.jsx
+++ src/components/IncomeTable/TableRow.jsx
@@ -4,25 +4,25 @@ const TableRow = () => {
   return (
     <Fragment>
       <tr>
-        <td className="border px-4 py-2">Taxable Income</td>
-        <td className="border px-4 py-2">100</td>
-        <td className="border px-4 py-2">858</td>
-        <td className="border px-4 py-2">858</td>
-        <td className="border px-4 py-2">858</td>
+        <td className="border px-10 py-2">Taxable Income</td>
+        <td className="border px-10 py-2">100</td>
+        <td className="border px-10 py-2">858</td>
+        <td className="border px-10 py-2">858</td>
+        <td className="border px-10 py-2">858</td>
       </tr>
       <tr>
-        <td className="border px-4 py-2">Pay</td>
-        <td className="border px-4 py-2">200</td>
-        <td className="border px-4 py-2">858</td>
-        <td className="border px-4 py-2">858</td>
-        <td className="border px-4 py-2">858</td>
+        <td className="border px-10 py-2">Pay</td>
+        <td className="border px-10 py-2">200</td>
+        <td className="border px-10 py-2">858</td>
+        <td className="border px-10 py-2">858</td>
+        <td className="border px-10 py-2">858</td>
       </tr>
       <tr>
-        <td className="border px-4 py-2">Taxes</td>
-        <td className="border px-4 py-2">300</td>
-        <td className="border px-4 py-2">858</td>
-        <td className="border px-4 py-2">858</td>
-        <td className="border px-4 py-2">858</td>
+        <td className="border px-10 py-2">Taxes</td>
+        <td className="border px-10 py-2">300</td>
+        <td className="border px-10 py-2">858</td>
+        <td className="border px-10 py-2">858</td>
+        <td className="border px-10 py-2">858</td>
       </tr>
     </Fragment>
   );
