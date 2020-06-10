3d7b3c43163e52ecb0f5403f966321e944c7a6a7
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-06-10 21:29:30 +0930

    Updated Table

--- src/components/IncomeTable/Table.jsx
+++ src/components/IncomeTable/Table.jsx
@@ -7,9 +7,6 @@ export const Table = () => {
       <thead>
         <tr>
           <th className="px-4 py-2"></th>
-          <th className="px-4 py-2">Weekly</th>
-          <th className="px-4 py-2">Fortnightly</th>
-          <th className="px-4 py-2">Monthly</th>
           <th className="px-4 py-2">Annually</th>
         </tr>
       </thead>
--- src/components/IncomeTable/TableRow.jsx
+++ src/components/IncomeTable/TableRow.jsx
@@ -13,27 +13,18 @@ export const TableRow = () => {
     <Fragment>
       <tr>
         <td className="border px-10 py-2">Pay</td>
-        <td className="border px-10 py-2">-</td>
-        <td className="border px-10 py-2">-</td>
-        <td className="border px-10 py-2">-</td>
         <td className="border px-10 py-2">
           <FormatNumbers value={pay} />
         </td>
       </tr>
       <tr>
         <td className="border px-10 py-2">Taxable Income</td>
-        <td className="border px-10 py-2">-</td>
-        <td className="border px-10 py-2">-</td>
-        <td className="border px-10 py-2">-</td>
         <td className="border px-10 py-2">
           <FormatNumbers value={income} />
         </td>
       </tr>
       <tr>
         <td className="border px-10 py-2">Taxes</td>
-        <td className="border px-10 py-2">-</td>
-        <td className="border px-10 py-2">-</td>
-        <td className="border px-10 py-2">-</td>
         <td className="border px-10 py-2">
           <FormatNumbers value={annualTax} />
         </td>
